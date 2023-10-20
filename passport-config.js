26e0180bc07ba2d53ce0259b620b361ca4b9ee29
seonmo <jeongahkim8174@gmail.com>
2023-10-20 00:17:28 -0700

    made the session work

---
+++ passport-config.js
@@ -0,0 +1,29 @@
+import passportLocal from 'passport-local';
+import bcrypt from 'bcrypt';
+const LocalStrategy = passportLocal.Strategy;
+
+export default function initalize(passport, getUserByUsername, getUserById){
+    const authenticateUser = async (username, password, done) => {
+        const user = getUserByUsername(username);
+        if (!user){
+            return done(null, false, {message: 'Incorrect username'});
+        }
+
+        try{
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user);
+            }else{
+                return done(null, false, {message: 'Incorrect password'});
+            }
+        } catch(e){
+            console.log(e);
+        }
+    }
+    passport.use(new LocalStrategy({usernameField: 'username'}, authenticateUser));
+    passport.serializeUser((user, done) => {
+        return done(null, user.id);
+    });
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id));
+    });
+}
--- server.js
+++ server.js
@@ -2,29 +2,58 @@ import express from 'express';
 import "dotenv/config.js";
 import cors from 'cors';
 import bcrypt from 'bcrypt';
+import initializePassport from './passport-config.js';
+import passport from 'passport';
+import flash from 'express-flash';
+import session from 'express-session';
+
 const app = express();
+
 const users = [];
+initializePassport(
+    passport,
+    (username) => users.find(user => user.username === username),
+    (id) => users.find(user => user.id === id)
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors({
     origin: 'http://localhost:5173', // Replace with your frontend's origin
-    credentials: true, // If your requests include credentials
+    credentials: true
 }));
 
-app.post('/signUp',async (req,res)=>{ 
+
+app.post('/signUp', async (req,res)=>{ 
     try{
         let password = await bcrypt.hash(req.body.inputPassword,10);
-        users.push({
-            id: Math.round(Math.random()*1000000),
+        let newUser = {
+            id: Date.now().toString(),
             username: req.body.inputUsername,
             password: password,
             gender: req.body.inputGender
-        })
+        }
+        users.push(newUser)
+        req.login(newUser, (err) => {
+            if (err) {
+                console.log(err);
+                res.json({loggedIn: false});
+            } else {
+                console.log(req.user);
+                res.json({loggedIn: true, username: req.user.username, gender: req.user.gender });
+            }
+        });
     }catch(e){
         console.log(e);
     }
-    res.json(users[0])
 })
 
 app.listen(process.env.PORT_NUMBER, ()=>console.log(`Listening on port ${process.env.PORT_NUMBER}`))
